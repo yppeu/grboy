74de889538a9dd0355c2d20de9e6d5c27610861e
selly <yppeu@naver.com>
2023-11-15 22:25:10 +0900

    1115

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
+// 0. a클릭시 위로 튕기는 이벤트제거
+$(document).on('click', 'a[href="#"]', function(e){
+    e.preventDefault();
+});
+
+
+
 // 1. approach 영역
 $(window).on('scroll resize', function(){
     console.log(1);
     var scrollPos = 0;
     scrollPos = $(document).scrollTop();
     fix();
+    fixHeader();
 
     function fix(){
         if(scrollPos > 1150){
@@ -12,6 +20,13 @@ $(window).on('scroll resize', function(){
             $('.fix .text').removeClass('on');
         }if(scrollPos > 2700){
             $('.fix .text').removeClass('on');
+        }
+    }
+    function fixHeader(){
+        if(scrollPos > 80){
+            $('header').addClass('on');
+        }else{
+            $('header').removeClass('on');
         }
     }
 });
